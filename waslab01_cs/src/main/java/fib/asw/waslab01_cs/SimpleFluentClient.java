d57c963fa5df4254c705b866adf6709f9aabc0f0
gbeltran <gbeltran@p_gbeltran1.smassa.es>
2023-09-13 22:05:30 +0200

    tasca #4 finalitzada

--- waslab01_cs/src/main/java/fib/asw/waslab01_cs/SimpleFluentClient.java
+++ waslab01_cs/src/main/java/fib/asw/waslab01_cs/SimpleFluentClient.java
@@ -1,5 +1,6 @@
 package fib.asw.waslab01_cs;
 
+import org.apache.hc.client5.http.fluent.Form;
 import org.apache.hc.client5.http.fluent.Request;
 
 //This code uses the Fluent API
@@ -10,6 +11,9 @@ public class SimpleFluentClient {
 	public final static void main(String[] args) throws Exception {
   	
   	/* Insert code for Task #4 here */
+		
+	String id = Request.post(URI).bodyForm(Form.form().add("author", "Gisela").add("tweet_text", "Hola").build()).addHeader("Accept", "text/plain").execute().returnContent().asString();
+	System.out.println(id);
   	
   	System.out.println(Request.get(URI).addHeader("Accept", "text/plain").execute().returnContent());
   	
